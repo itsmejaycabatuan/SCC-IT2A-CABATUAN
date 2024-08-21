dc77807a90a78c46559235226e65172a7942d123
itsgale <mcetolentino0911@gmail.com>
2024-08-21 12:09:21 +0800

    Product Update

--- src/firstjavaact/FirstJavaAct.java
+++ src/firstjavaact/FirstJavaAct.java
@@ -8,10 +8,22 @@ public class FirstJavaAct {
 
     
     public static void main(String[] args) {
-       
-        Grade grd1= new Grade();
+       /*
+       Grade grd1= new Grade();
         grd1.getGrade();
-        
+        */
+       
+       Product pr = new Product();
+       
+         System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10.5s\n",
+                 "Product ID", "Name" , "Price" , "Stocks" , "Status" , "Tep");
+       
+         pr.AddProduct(10011, "Soap", 20, 10, 200);
+       pr.viewProduct();
+       
+       Product pr1 = new Product();
+       pr1.AddProduct(10012, "Cards", 50, 0, 0);
+       pr1.viewProduct();
     }
     
 }
